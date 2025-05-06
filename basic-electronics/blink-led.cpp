5c2105ddecfe599a5da7f7786f4e79731b378a74
valen <18544814+xvk-64@users.noreply.github.com>
2025-05-06 14:29:13 +0930

    Add blink LED on breadboard.

---
+++ basic-electronics/blink-led.cpp
@@ -0,0 +1,25 @@
+#include "boards/pico.h"
+#include "pico/stdlib.h"
+
+// Our LED is connected to GP15 (Pin 20)
+constexpr uint led_pin = 15;
+
+[[noreturn]] int main() {
+    // Initialise the pin and make it an output.
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    while (true) {
+        // Put 3.3V at the pin.
+        gpio_put(led_pin, true);
+
+        // Wait 500ms.
+        sleep_ms(500);
+
+        // Put 0V at the pin.
+        gpio_put(led_pin, false);
+
+        // Wait some more.
+        sleep_ms(500);
+    }
+}
