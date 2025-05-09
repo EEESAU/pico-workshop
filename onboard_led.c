538d2c6ec2bd12e547941396d442c73bae938596
valen <18544814+xvk-64@users.noreply.github.com>
2025-05-09 16:10:06 +0930

    Added comments to onboard_led

--- onboard_led.c
+++ onboard_led.c
@@ -3,8 +3,10 @@
 #include "pico/cyw43_arch.h"
 
 int main() {
-    // Turn on the onboard LED to verify our toolchain
+    // On the Pico W series, the onboard LED needs to be accessed through the CYW43 wireless module.
+    // Initialise the wireless module.
     cyw43_arch_init();
 
+    // Tell the wireless module to turn on the LED.
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
 }
